a718e7fee2fa0494df724abe1e43347d7f21297a
gaston <gastoncer92@gmail.com>
2021-05-08 05:05:26 -0300

    commit 1

--- supercalculadora.py
+++ supercalculadora.py
@@ -1,1 +1,17 @@
-#supercalculadora
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtGui, QtWidgets
+
+
+class Main(QtWidgets.QApplication):
+
+    def __init__(self, *args, **kwargs):
+        super(Main, self).__init__(*args, **kwargs)
+        self.loadUi('main.ui')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    GUI = Main()
+    GUI.show()
+    sys.exit(app.exec_())
